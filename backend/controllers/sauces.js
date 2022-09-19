092437bda7a08841be06e0fd4b8662e1f5649657
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-19 17:11:25 +0200

    user & path

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,14 +1,18 @@
 const Sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
-    delete req.body._id;
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    delete sauceObject._userid;
     const sauce = new Sauce({
-        ...req.body
+        ...sauceObject, 
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
-        .catch(error => res.status(400).json({ error }));
-}
+    .then(() => { res.status(201).json({message: 'Sauce enregistrée !'})})
+    .catch(error => { res.status(400).json( { error })})
+};
 
 exports.modifySauce = (req, res, next) => {
     Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
