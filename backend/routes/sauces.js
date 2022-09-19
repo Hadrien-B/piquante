add9513c7d168dd06d46ae2397d7e6e4afdabada
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-19 17:12:02 +0200

    user & path

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const auth = require('../middleware/auth');//middleware d'authentification
 const router = express.Router();
+const multer = require('../middleware/multer-config');
+
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('/', saucesCtrl.createSauce);//route POST permettant de créer une nouvelle sauce
-router.put('/:id', saucesCtrl.modifySauce);//route PUT permettant de modifier une nouvelle sauce
-router.delete('/:id', saucesCtrl.deleteSauce);//route POST permettant de supprimer une nouvelle sauce
-router.get('/:id', saucesCtrl.getOneSauce);//route POST permettant de récupérer une sauce
-router.get('/', saucesCtrl.getAllSauce);//route POST permettant de récupérér toutes les sauces
+router.post('/',auth, multer, saucesCtrl.createSauce);//route POST permettant de créer une nouvelle sauce
+router.put('/:id',auth, saucesCtrl.modifySauce);//route PUT permettant de modifier une nouvelle sauce
+router.delete('/:id',auth, saucesCtrl.deleteSauce);//route POST permettant de supprimer une nouvelle sauce
+router.get('/:id',auth, saucesCtrl.getOneSauce);//route POST permettant de récupérer une sauce
+router.get('/',auth, saucesCtrl.getAllSauce);//route POST permettant de récupérér toutes les sauces
 
 module.exports = router;
