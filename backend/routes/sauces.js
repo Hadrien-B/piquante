279f3b58317031b475fba430d52490641190c693
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-20 11:05:57 +0200

    path multer

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config');
 const saucesCtrl = require('../controllers/sauces');
 
 router.post('/',auth, multer, saucesCtrl.createSauce);//route POST permettant de créer une nouvelle sauce
-router.put('/:id',auth, saucesCtrl.modifySauce);//route PUT permettant de modifier une nouvelle sauce
+router.put('/:id',auth, multer, saucesCtrl.modifySauce);//route PUT permettant de modifier une nouvelle sauce
 router.delete('/:id',auth, saucesCtrl.deleteSauce);//route POST permettant de supprimer une nouvelle sauce
 router.get('/:id',auth, saucesCtrl.getOneSauce);//route POST permettant de récupérer une sauce
 router.get('/',auth, saucesCtrl.getAllSauce);//route POST permettant de récupérér toutes les sauces
