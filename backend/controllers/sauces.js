e0c58736cb14f6fb19e25d3e618047ebf997977b
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-22 12:22:21 +0200

    correction function delete

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -31,16 +31,12 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            if (sauce.userId != req.auth.userId) {
-                res.status(401).json({ message: 'Action non autorisée' });
-            } else {
                 const filename = sauce.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, () => {
                     Sauce.deleteOne({ _id: req.params.id })
                         .then(() => { res.status(200).json({ message: 'La sauce a été supprimée !' }) })
                         .catch(error => res.status(401).json({ error }));
                 });
-            }
         })
         .catch(error => {
             res.status(500).json({ error });
