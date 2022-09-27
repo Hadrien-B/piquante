b9c57542a85548229dcccd9af597fe98c1253bef
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-27 16:33:16 +0200

    ajout commentaires

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
+    if (req.body.userId && req.body.userId !== userId) {//Vérification de l'ID utilisateur par rapport au TOKEN
       throw new Error('Identifiant invalide');
     } else {
       next();
