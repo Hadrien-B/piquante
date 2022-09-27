10747456bfbeb284db38ac50b2ea8ac90fe8ade9
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-27 17:47:58 +0200

    ajout router pour le contrôle de l'email et du mot de passe

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const checkPassword = require("../middleware/check-password")
+const checkEmail = require("../middleware/check-email")
 const route = require('./sauces');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', checkEmail, checkPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.put('/login', userCtrl.login);
 
