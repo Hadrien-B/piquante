87d89c142f4983d33a8ab3544d5e0a0b056e20d7
Hadrien Borie <borie.hadrien@gmail.com>
2022-09-27 15:44:55 +0200

    ajout commentaires

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,13 @@
 const mongoose =require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+
 //Modèle utilisateur
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);//Plug-in permettant qu'un e-mail ne soit pas utilisé deux fois comme identifiant
+userSchema.plugin(uniqueValidator);//Plug-in vérifiant qu'un e-mail ne soit pas utilisé deux fois comme identifiant
 
 module.exports = mongoose.model('User', userSchema);
